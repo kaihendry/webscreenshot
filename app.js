8ac32b53ee14f2b05cbcfc9c2b2f7238603c12d2
Kai Hendry <hendry@webconverger.com>
2020-03-03 13:51:44 +0800

    Initial

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const playwright = require('playwright')
+// const { Logger } = require('up')
+const app = express()
+
+const { PORT = 3000 } = process.env
+
+// const log = new Logger()
+
+app.get('/', async (req, res) => {
+  let url = req.query.url
+  if (!url) { return }
+
+  // log.info('screenshot', { url })
+
+  const browser = await playwright['firefox'].launch()
+  const context = await browser.newContext()
+  const page = await context.newPage()
+  await page.goto(url)
+  const screenshotBuffer = await page.screenshot()
+  await browser.close()
+  // const img = screenshotBuffer.toString('base64')
+
+  res.writeHead(200, {
+    'Content-Type': 'image/png',
+    'Content-Length': screenshotBuffer.length
+  })
+  res.end(screenshotBuffer)
+})
+
+console.log('listening on %s', PORT)
+app.listen(PORT)
